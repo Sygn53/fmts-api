46eb70d092a83505edbd2d4af4af6c23c47e13c8
oktaysaygin <oktaysaygin17@gmail.com>
2024-12-18 15:19:22 +0300

    json formatter

--- routes/users.js
+++ routes/users.js
@@ -20,5 +20,4 @@ router.get('/102', (req, res) => {
     res.send('this is user 102 route');// this gets executed when user visit http://localhost:3000/user/102
 });
 
-// export the router module so that server.js file can use it
 module.exports = router;
