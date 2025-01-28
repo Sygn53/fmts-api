208f847aeb836e5fd2fd14c458a5bc3201576890
oktaysaygin <oktaysaygin17@gmail.com>
2025-01-28 15:45:43 +0300

    export

--- api/index.js
+++ api/index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
 
-const usersRoute = require('../routes/users');
+// const usersRoute = require('../routes/users');
 const homeRoute = require('../routes/home');
-const amazonData = require('../routes/amazonData');
-const wordsData = require('../routes/wordsData');
+// const amazonData = require('../routes/amazonData');
+// const wordsData = require('../routes/wordsData');
 
 // URL Encoded veri ayrıştırıcı (Opsiyonel)
 app.use(express.json());
@@ -12,9 +12,9 @@ app.use(express.urlencoded({ extended: true }));
 
 // app.use('/monitoring', monitorRoute);
 app.use('/' ,homeRoute);
-app.use('/users', usersRoute);
-app.use('/amazonData', amazonData);
-app.use('/words', wordsData);
+// app.use('/users', usersRoute);
+// app.use('/amazonData', amazonData);
+// app.use('/words', wordsData);
 
 // const ipAddress = "10.34.10.138"; // IP adresiniz
 // const ipAddress = "10.34.10.138"; // IP adresiniz
