2ce07668f54b5a446a07d1d07ba518ffa47668d8
oktaysaygin <oktaysaygin17@gmail.com>
2025-02-06 11:33:07 +0300

    while response

--- api/Headers.js
+++ api/Headers.js
@@ -1,5 +1,7 @@
+import randomUseragent from "random-useragent";
+
 const headers1 = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+    "User-Agent": randomUseragent.getRandom(),
     "Accept-Language": "en-US,en;q=0.9",
     "Accept-Encoding": "gzip, deflate, br",
     "Referer": "https://www.google.com/",
@@ -8,7 +10,7 @@ const headers1 = {
 };
 
 const headers2 = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:118.0) Gecko/20100101 Firefox/118.0",
+    "User-Agent": randomUseragent.getRandom(),
     "Accept-Language": "en-US,en;q=0.5",
     "Accept-Encoding": "gzip, deflate, br",
     "Referer": "https://www.bing.com/",
@@ -17,7 +19,7 @@ const headers2 = {
 };
 
 const headers3 = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 Edg/120.0.0.0",
+    "User-Agent": randomUseragent.getRandom(),
     "Accept-Language": "en-US,en;q=0.8",
     "Accept-Encoding": "gzip, deflate, br",
     "Referer": "https://www.bing.com/",
@@ -26,7 +28,7 @@ const headers3 = {
 };
 
 const headers4 = {
-    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 13_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+    "User-Agent": randomUseragent.getRandom(),
     "Accept-Language": "en-US,en;q=0.9",
     "Accept-Encoding": "gzip, deflate, br",
     "Referer": "https://www.yahoo.com/",
@@ -35,7 +37,7 @@ const headers4 = {
 };
 
 const headers5 = {
-    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 13_3) AppleWebKit/537.36 (KHTML, like Gecko) Version/17.0 Safari/537.36",
+    "User-Agent": randomUseragent.getRandom(),
     "Accept-Language": "en-US,en;q=0.9",
     "Accept-Encoding": "gzip, deflate, br",
     "Referer": "https://www.apple.com/",
@@ -44,7 +46,7 @@ const headers5 = {
 };
 
 const headers6 = {
-    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+    "User-Agent": randomUseragent.getRandom(),
     "Accept-Language": "en-US,en;q=0.9",
     "Accept-Encoding": "gzip, deflate, br",
     "Referer": "https://www.google.com/",
@@ -53,16 +55,16 @@ const headers6 = {
 };
 
 const headers7 = {
-    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:118.0) Gecko/20100101 Firefox/118.0",
-    "Accept-Language": "en-US,en;q=0.8",
+    "User-Agent": randomUseragent.getRandom(),
+    "Accept-Language": "en-US,en;q=0.9",
     "Accept-Encoding": "gzip, deflate, br",
-    "Referer": "https://www.debian.org/",
+    "Referer": "https://www.google.com/",
     "DNT": "1",
     "Connection": "keep-alive"
 };
 
 const headers8 = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36",
+    "User-Agent": randomUseragent.getRandom(),
     "Accept-Language": "en-US,en;q=0.9",
     "Accept-Encoding": "gzip, deflate, br",
     "Referer": "https://www.google.com/",
@@ -71,25 +73,7 @@ const headers8 = {
 };
 
 const headers9 = {
-    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36",
-    "Accept-Language": "en-US,en;q=0.9",
-    "Accept-Encoding": "gzip, deflate, br",
-    "Referer": "https://www.google.com/",
-    "DNT": "1",
-    "Connection": "keep-alive"
-};
-
-const headers10 = {
-    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36",
-    "Accept-Language": "en-US,en;q=0.9",
-    "Accept-Encoding": "gzip, deflate, br",
-    "Referer": "https://www.google.com/",
-    "DNT": "1",
-    "Connection": "keep-alive"
-};
-
-const headers11 = {
-    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 14_2) AppleWebKit/537.36 (KHTML, like Gecko) Version/18.0 Safari/537.36",
+    "User-Agent": randomUseragent.getRandom(),
     "Accept-Language": "en-US,en;q=0.9",
     "Accept-Encoding": "gzip, deflate, br",
     "Referer": "https://www.apple.com/",
@@ -97,8 +81,8 @@ const headers11 = {
     "Connection": "keep-alive"
 };
 
-const headers12 = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 11.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Edge/121.0.0.0",
+const headers10 = {
+    "User-Agent": randomUseragent.getRandom(),
     "Accept-Language": "en-US,en;q=0.9",
     "Accept-Encoding": "gzip, deflate, br",
     "Referer": "https://www.bing.com/",
@@ -106,8 +90,8 @@ const headers12 = {
     "Connection": "keep-alive"
 };
 
-const headers13 = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:121.0) Gecko/20100101 Firefox/121.0",
+const headers11 = {
+    "User-Agent": randomUseragent.getRandom(),
     "Accept-Language": "en-US,en;q=0.8",
     "Accept-Encoding": "gzip, deflate, br",
     "Referer": "https://www.mozilla.org/",
@@ -115,8 +99,8 @@ const headers13 = {
     "Connection": "keep-alive"
 };
 
-const headers14 = {
-    "User-Agent": "Mozilla/5.0 (iPad; CPU OS 18_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.0 Mobile/15E148 Safari/604.1",
+const headers12 = {
+    "User-Agent": randomUseragent.getRandom(),
     "Accept-Language": "en-US,en;q=0.9",
     "Accept-Encoding": "gzip, deflate, br",
     "Referer": "https://www.apple.com/",
@@ -124,7 +108,7 @@ const headers14 = {
     "Connection": "keep-alive"
 };
 
-const headersList = [headers1, headers2, headers3, headers4, headers5, headers6, headers7, headers8, headers9, headers10, headers11, headers12, headers13, headers14];
+const headersList = [headers1, headers2, headers3, headers4, headers5, headers6, headers7, headers8, headers9, headers10, headers11, headers12];
 
 export const getRandomHeaders = () => {
     return headersList[Math.floor(Math.random() * headersList.length)];
--- components/bestSeller.js
+++ components/bestSeller.js
@@ -17,96 +17,106 @@ const bestSellerUrl = 'https://www.amazon.com/Best-Sellers/zgbs';
 // };
 
 const bestSeller = async () => {
-    try {
-        const { data } = await axios.get(bestSellerUrl, {
-            headers: headers,
-            // proxy: proxyConfig
-        });
-
-        // Sayfanın içeriğini JSDOM ile yükle
-        const dom = new JSDOM(data);
-        const document = dom.window.document;
-
-        // Best Seller ürünlerini seç
-        const products = [];
-
-        const categories = [];
-        const categoryTitle = document.querySelectorAll('h2.a-carousel-heading');
-        categoryTitle.forEach((item, index) => {
-            categories.push(item.textContent.trim());
-        });
-
-        const carouselElements = document.querySelectorAll('.a-carousel-viewport');
-        carouselElements.forEach((element, index) => {
-            const title = [];
-            const link = [];
-            const image = [];
-            const rating = [];
-            const price = [];
-            const items = [];
-
-            // Title
-            const titleElements = element.querySelectorAll('.p13n-sc-truncate-desktop-type2');
-            titleElements.forEach((item) => {
-                title.push(item.textContent.trim());
+    let again = true;
+    while (again) {
+        try {
+            const { data } = await axios.get(bestSellerUrl, {
+                headers: headers,
+                // proxy: proxyConfig
             });
 
-            // Link
-            const linkElements = element.querySelectorAll('.a-link-normal');
-            linkElements.forEach((item, ind) => {
-                if (ind < 6) {
-                    link.push(item.getAttribute('href'));
+            // Sayfanın içeriğini JSDOM ile yükle
+            const dom = new JSDOM(data);
+            const document = dom.window.document;
+
+            // Best Seller ürünlerini seç
+            const products = [];
+
+            const categories = [];
+            const categoryTitle = document.querySelectorAll('h2.a-carousel-heading');
+            categoryTitle.forEach((item, index) => {
+                categories.push(item.textContent.trim());
+            });
+
+            const carouselElements = document.querySelectorAll('.a-carousel-viewport');
+            carouselElements.forEach((element, index) => {
+                const title = [];
+                const link = [];
+                const image = [];
+                const rating = [];
+                const price = [];
+                const items = [];
+
+                // Title
+                const titleElements = element.querySelectorAll('.p13n-sc-truncate-desktop-type2');
+                titleElements.forEach((item) => {
+                    title.push(item.textContent.trim());
+                });
+
+                // Link
+                const linkElements = element.querySelectorAll('.a-link-normal');
+                linkElements.forEach((item, ind) => {
+                    if (ind < 6) {
+                        link.push(item.getAttribute('href'));
+                    }
+                });
+
+                // Image
+                const imgElements = element.querySelectorAll('img');
+                imgElements.forEach((img) => {
+                    const imgSrc = img.getAttribute('src');
+                    if (imgSrc) {
+                        image.push(imgSrc);
+                    }
+                });
+
+                // Rating
+                const ratingElements = element.querySelectorAll('.a-icon-alt');
+                ratingElements.forEach((item) => {
+                    rating.push(item.textContent.trim());
+                });
+
+                // Price
+                const priceElements = element.querySelectorAll('[class*="p13n-sc-price"]');
+                priceElements.forEach((item, index) => {
+                    // Eğer öğe bir <style> etiketi içinde değilse işle
+                    if (!item.closest('style') && !price.includes(item.textContent.trim())) {
+                        price.push(item.textContent.trim());
+                    }
+                });
+
+                for (let i = 0; i < 6; i++) {
+                    let obj = {
+                        title: title[i] || '',
+                        link: link[i] || '',
+                        image: image[i] || '',
+                        rating: rating[i] || 'No rating',
+                        price: price[i] || 'No price',
+                    };
+                    items.push(obj);
                 }
-            });
 
-            // Image
-            const imgElements = element.querySelectorAll('img');
-            imgElements.forEach((img) => {
-                const imgSrc = img.getAttribute('src');
-                if (imgSrc) {
-                    image.push(imgSrc);
-                }
+                products.push({
+                    categories: categories[index],
+                    index: index,
+                    items: items,
+                });
             });
-
-            // Rating
-            const ratingElements = element.querySelectorAll('.a-icon-alt');
-            ratingElements.forEach((item) => {
-                rating.push(item.textContent.trim());
-            });
-
-            // Price
-            const priceElements = element.querySelectorAll('[class*="p13n-sc-price"]');
-            priceElements.forEach((item, index) => {
-                // Eğer öğe bir <style> etiketi içinde değilse işle
-                if (!item.closest('style') && !price.includes(item.textContent.trim())) {
-                    price.push(item.textContent.trim());
-                }
-            });
-
-            for (let i = 0; i < 6; i++) {
-                let obj = {
-                    title: title[i] || '',
-                    link: link[i] || '',
-                    image: image[i] || '',
-                    rating: rating[i] || 'No rating',
-                    price: price[i] || 'No price',
-                };
-                items.push(obj);
+            if (products[0]?.items[0].title === '' || products[0]?.items[0].price === 'No price') {
+                again = true;
+                console.log("girdi...");
+            } else {
+                again = false;
+                return products;
             }
-
-            products.push({
-                categories: categories[index],
-                index: index,
-                items: items,
-            });
-        });
-
-        return products;
-
-    } catch (error) {
-        console.error("Genel Hata:", error.message);
-        return "Genel Hata:" + error.message
+        } catch (error) {
+            console.error("Genel Hata:", error.message);
+            again = true;
+            // return "Genel Hata:" + error.message
+        }
     }
+
+
 };
 
 module.exports = { bestSeller };
